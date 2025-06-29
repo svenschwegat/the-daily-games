fc5c679c9fafb57342da29b6a330c803e081b9a0
Sven Schwegat <sven.schwegat+github@gmail.com>
2025-06-29 11:48:45 +0200

    Changed to a nicer grid layout with hover

--- frontend/src/components/GameGrid.tsx
+++ frontend/src/components/GameGrid.tsx
@@ -21,9 +21,19 @@ export default function GameGrid({games}: GameGridProps) {
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       {games.map((game: Game) => (
-        <div key={game.id} className="p-4 border rounded-lg m-2">
-          <a href={game.link} target="_blank" className="text-xl font-bold">{game.name}</a>
-          <p className="text-gray-700">{game.description}</p>
+        <div 
+          key={game.id} 
+          className="p-4 bg-slate-300 rounded-lg m-2 relative h-27 
+            hover:bg-slate-400 hover:-translate-y-1 hover:scale-105
+            transition duration-500 ease-in-out">
+          <a 
+            href={game.link} 
+            target="_blank" 
+            className="text-xl font-bold text-gray-700">
+              {game.name}
+            <span className="absolute w-full h-full top-0 left-0 z-1"> </span>
+          </a>
+          <p className="text-gray-700 line-clamp-2">{game.description}</p>
         </div>
       ))}
     </div>
