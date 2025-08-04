0fc4d0a958d6bebfedbfc3acfa71ef699850b562
Sven Schwegat <sven.schwegat+github@gmail.com>
2025-08-04 16:31:45 +0200

    Fixed Chips node Warnings

--- frontend/src/components/FilterChips.tsx
+++ frontend/src/components/FilterChips.tsx
@@ -6,25 +6,27 @@ export default function FilterChips({ filterContent, selectedFilterIds, dispatch
   const onClose = (key: FilterKey, id: number) => {
     dispatch({ type: 'TOGGLE_FILTER_VALUE', key: key as FilterKey, value: id });
   }
-  
+
   return (
-    <div id="FilterChips" className="flex flex-wrap gap-2">
+    <div id="FilterChips">
       {Object.entries(selectedFilterIds).map(([key, ids]) => (
-        <>
+        <div key={key} className="flex flex-wrap gap-1">
           {ids.map(id => {
             const filterItem = filterContent[key as FilterKey].find(f => f.id === id && id !== 0);
             return filterItem ? (
-              <Chip 
-                size="sm" 
-                color="secondary"
-                startContent={<FilterIcon type={key as FilterKey} width={20} height={20} color="#ffffff" />}
-                onClose={() => onClose(key as FilterKey, id)}
-              >
-                {filterItem.name}
-              </Chip>
+              <div id={key + id} key={key + id}>
+                <Chip
+                  size="sm"
+                  color="secondary"
+                  startContent={<FilterIcon type={key as FilterKey} width={20} height={20} color="#ffffff" />}
+                  onClose={() => onClose(key as FilterKey, id)}
+                >
+                  {filterItem.name}
+                </Chip>
+              </div>
             ) : null;
           })}
-        </>
+        </div>
       ))}
     </div>
   );
