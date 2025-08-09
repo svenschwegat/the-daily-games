36aa493f6459c9e75c4b51218e5214fcdf86c662
Sven Schwegat <sven.schwegat+github@gmail.com>
2025-08-09 11:56:35 +0200

    Changed the game grid card

--- frontend/src/components/GameGrid.tsx
+++ frontend/src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import GameGridImage from './GameGridImage';
+import GameGridCard from './GameGridCard';
 import type { Game, GameGridProps } from '../types/GameTypes';
 import type { FilterKey } from '../types/FilterTypes';
 import { filterNamingScheme } from '@/utils/FilterNamingScheme';
@@ -35,25 +35,9 @@ export default function GameGrid({ initialGames, filters }: GameGridProps) {
   }
 
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4 p-4 pl-20 pr-20">
+    <div className="grid xl:grid-cols-3 lg:grid-cols-2 gap-4 p-4 pl-20 pr-20">
       {games.map((game: Game) => (
-        <div
-          id={game.id + game.name}
-          key={game.id}
-          className="p-4 bg-white border-1 border-slate-300 rounded-lg m-2 relative h-110
-            hover:bg-slate-400 hover:-translate-y-1 hover:scale-105
-            transition duration-500 ease-in-out"
-        >
-          <GameGridImage game={game} />
-          <a
-            href={game.link}
-            target="_blank"
-            className="text-xl font-bold text-gray-700 truncate">
-            {game.name}
-            <span className="absolute w-full h-full top-0 left-0 z-1"> </span>
-          </a>
-          <p className="text-gray-700 line-clamp-2">{game.description}</p>
-        </div>
+        <GameGridCard game={game} key={game.id}/>
       ))}
     </div>
   );
---
+++ frontend/src/components/GameGridCard.tsx
@@ -0,0 +1,33 @@
+import type { Game } from '../types/GameTypes';
+import GameGridImage from './GameGridImage';
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link } from '@heroui/react'
+
+export default function GameGridCard({ game }: { game: Game }) {
+  return (
+    <Card isPressable>
+      <CardHeader>
+        <GameGridImage game={game} />
+      </CardHeader>
+      <CardBody>
+        <div className="text-xl font-bold text-gray-700 truncate text-center">
+          {game.name}
+        </div>
+        <div className="text-center line-clamp-2">
+          {game.description}
+        </div>
+
+      </CardBody>
+      <Divider />
+      <CardFooter className="flex gap-4 justify-center">
+        <Link
+          isExternal
+          showAnchorIcon
+          isBlock
+          href={game.link}
+        >
+          Play {game.name}
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+}
