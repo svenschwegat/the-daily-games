1b7db6eedb880dafedef2bc2e6338cf8f80a9cc9
Sven Schwegat <sven.schwegat+github@gmail.com>
2025-08-31 20:10:22 +0200

    Added debugging line

--- frontend/src/components/GameGridImage.tsx
+++ frontend/src/components/GameGridImage.tsx
@@ -3,8 +3,8 @@ import type { GameGridImageProps } from '@/types/GameTypes';
 import Image from 'next/image';
 
 export default function GameGridImage({ game }: GameGridImageProps) {
-  // const imageBaseUrl = `/supabase/storage/v1/object/public/pictures/Preview_`;
-  const imageBaseUrl = '/Preview_';
+  const imageBaseUrl = `/supabase/storage/v1/object/public/pictures/Preview_`;
+  // const imageBaseUrl = '/Preview_'; // For debugging locally without supabase
   const [src, setSrc] = React.useState(`${imageBaseUrl}Empty.png`);
 
   React.useEffect(() => {
